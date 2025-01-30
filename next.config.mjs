bdcb1375544f6ab0675f6f5c42b1f1d550739524
nayanbamnote <nareshbamnote52@gmail.com>
2025-01-31 05:25:49 +0530

    updated things 3.O

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_PUBLIC_KEY: "61T9KLt4xU7IWCVl3", // Add your actual public key here
-  },
 };
 
 export default nextConfig;
